e4824d0dd1cff5ba51a46145e21f05e60d840e7a
Karl-Reimond Kõrs <71008636+karlreimond@users.noreply.github.com>
2021-10-01 10:10:47 +0200

    Delete karlbackend directory

--- karlbackend/src/main/java/ee/karl_reimond/backend/repostiroy/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.karl_reimond.backend.repostiroy;
-
-import ee.karl_reimond.backend.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
