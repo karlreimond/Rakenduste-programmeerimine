df6f0503f778460e60845ee98a898eb8f542bbf9
karlreimond <karlreim@tlu.ee>
2021-10-01 09:25:52 +0200

    kodutoo2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
-import Item from './components/Item';
+import {Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import AddItem from './pages/AddItem';
+import Navbar from './components/Navbar';
+import Categories from './pages/Categories';
+import AddCategory from './pages/AddCategory';
+
 function App() {
   return (
-    <div>
-      Tere
-      <Item />
-      <Item />
-    </div>
+      <div>
+        <Navbar />
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/add-item'>
+          <AddItem />
+        </Route>
+        <Route path='/add-category'>
+          <AddCategory />
+        </Route>
+        <Route path="/categories">
+          <Categories/>
+        </Route>
+      </div>
   );
 }
 
